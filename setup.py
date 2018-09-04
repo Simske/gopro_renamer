4f969d2c1d39c4b23c67db950d8a73466e42e7ff
Simske <mail@simske.com>
2018-09-04 13:58:00 +0200

    Read version for setup from script file

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from setuptools import setup
+from gopro_chapter_renamer import __version__
 
 setup(
     name = 'gopro_renamer',
     author = 'Kevin Ha',
-    version = '0.3.3',
+    version = __version__,
     py_modules = ['gopro_chapter_renamer'],
     entry_points = {
         'console_scripts': ['gopro_renamer = gopro_chapter_renamer:main']
